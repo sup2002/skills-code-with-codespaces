1cc88a3246fd091ec92e8a85116c39e9fd5fa7ab
sup2002 <128950814+sup2002@users.noreply.github.com>
2026-03-10 08:38:23 +0000

    feat: Add features, extensions, and postCreate script

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Devcontainer setup complete"
+
+sudo apt-get update
+sudo apt-get install sl
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
